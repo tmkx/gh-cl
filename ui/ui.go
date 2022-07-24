c720024ea32416b77e59286f5079a5fc6a0865a1
Tmk <i@tmk.im>
2022-07-24 15:27:25 +0800

    Force quit with q/ctrl+c

--- ui/ui.go
+++ ui/ui.go
@@ -87,7 +87,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "esc", "ctrl+c":
+		case "q", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		case "esc":
 			if m.state == showChangelog && msg.String() != "ctrl+c" {
 				m.state = choosingRelease
 				return m, nil
